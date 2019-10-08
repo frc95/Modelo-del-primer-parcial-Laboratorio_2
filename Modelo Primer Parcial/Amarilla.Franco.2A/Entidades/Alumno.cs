05d61e9fbb008a50e6b62299c2de2e98b50e3acf
frc95 <36265058+frc95@users.noreply.github.com>
2019-10-08 11:55:09 -0300

    Add files via upload

---
+++ Modelo Primer Parcial/Amarilla.Franco.2A/Entidades/Alumno.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Alumno
+    {
+        #region ATRIBUTOS
+        private string _apellido;
+        private int _legajo;
+        private string _nombre;
+        private float _nota;
+        #endregion
+
+        #region PROPIEDADES
+        public string Apellido
+        {
+            get { return this._apellido; }
+            set { this._apellido = value; }
+        }
+        public int Legajo
+        {
+            get { return this._legajo; }
+            set { this._legajo = value; }
+        }
+        public string Nombre
+        {
+            get { return this._nombre; }
+            set { this._nombre = value; }
+        }
+        public float Nota
+        {
+            get { return this._nota; }
+            set { this._nota = value; }
+        }
+        #endregion
+
+        #region CONSTRUCTORES
+        public Alumno()
+        {
+            this.Apellido = "";
+            this.Nombre = "";
+            this.Nota = 0;
+            this.Legajo = 0;
+        }
+        public Alumno(string apellido):this()
+        {
+            this.Apellido = apellido;
+        }
+        public Alumno(string apellido, string nombre):this(apellido)
+        {
+            this.Nombre = nombre;
+        }
+        public Alumno(int legajo, string nombre, string apellido):this(apellido,nombre)
+        {
+            this.Legajo = legajo;
+        }
+        public Alumno(int legajo, string nombre, string apellido, float nota):this(legajo,nombre,apellido)
+        {
+            this.Nota = nota;
+        }
+        #endregion
+
+        #region METODOS
+        private string Mostrar()
+        {
+            return this.Apellido+", "+this.Nombre+" - Legajo: "+this.Legajo+" - Nota: "+this.Nota+"\n";
+        }
+        public string Mostrar(Alumno alumno)
+        {
+            return alumno.Mostrar();
+        }
+        #endregion
+
+        #region OPERADORES
+        public static bool operator ==(Alumno a1, Alumno a2)
+        {
+            bool validar = false;
+            if(!Object.Equals(a1,null) && !Object.Equals(a2,null))
+            {
+                if(a1.Legajo==a2.Legajo)
+                {
+                    validar = true;
+                }
+            }
+            return validar;
+        }
+        public static bool operator !=(Alumno a1, Alumno a2)
+        {
+            return !(a1==a2);
+        }
+        #endregion
+    }
+}
---
+++ Modelo Primer Parcial/Amarilla.Franco.2A/Entidades/Materia.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Materia
+    {
+        #region ATRIBUTOS
+        private List<Alumno> _alumnos;
+        private EMateria _nombre;
+        private static Random _notaParaUnAlumno;
+        #endregion
+
+        #region PROPIEDADES
+        public List<Alumno> Alumnos
+        {
+            get { return this._alumnos; }
+            set { this._alumnos = value; }
+        }
+        public EMateria Nombre
+        {
+            get { return this._nombre; }
+            set { this._nombre = value; }
+        }
+        #endregion
+
+        #region OPERADORES DE CONVERSION
+        public static explicit operator String(Materia materia)
+        {
+            return materia.Mostrar();
+        }
+        public static implicit operator float(Materia m)
+        {
+            float total = 0;
+            foreach(Alumno alumno in m.Alumnos)
+            {
+                total = total + alumno.Nota;
+            }
+            total = total / m.Alumnos.Count;
+            return total;
+        }
+        public static implicit operator Materia(EMateria nombre)
+        {
+            Materia materia = new Materia(nombre);
+            
+            return materia;
+        }
+        #endregion
+
+        #region CONSTRUCTORES
+        private Materia()
+        {
+            this.Alumnos = new List<Alumno>();
+        }
+        static Materia()
+        {
+            Materia._notaParaUnAlumno = new Random();
+        }
+        private Materia(EMateria nombre):this()
+        {
+            this.Nombre = nombre;
+        }
+        #endregion
+
+        #region METODOS
+        public void CalificarAlumnos()
+        {
+            for(int i=0;i<this.Alumnos.Count;i++)
+            {
+                this.Alumnos[i].Nota = Materia._notaParaUnAlumno.Next(1,10);
+            }
+        }
+        private string Mostrar()
+        {
+            StringBuilder datos = new StringBuilder();
+            datos.AppendLine("\nMateria: " + this.Nombre);
+            datos.AppendLine("*****************************");
+            datos.AppendLine("*********ALUMNOS*************");
+            foreach (Alumno alumno in this.Alumnos)
+            {
+                datos.AppendLine(alumno.Mostrar(alumno));
+            }
+            return datos.ToString();
+        }
+        #endregion
+
+        #region OPERADORES DE == != + -
+        public static bool operator ==(Materia m, Alumno a)
+        {
+            bool validar = false;
+            if(!Object.Equals(m,null) && !Object.Equals(a,null))
+            {
+                foreach(Alumno alumno in m.Alumnos)
+                {
+                    if(alumno==a)
+                    {
+                        validar = true;
+                    }
+                }
+            }
+            return validar;
+        }
+        public static bool operator !=(Materia m, Alumno a)
+        {
+            return !(m==a);
+        }
+        public static Materia operator +(Materia m, Alumno a)
+        {
+            if(m!=a)
+            {
+                m.Alumnos.Add(a);
+                Console.WriteLine("Se agrego el alumno a la materia {0}!!!",m.Nombre);
+            }
+            else
+            {
+                Console.WriteLine("El alumno ya esta en la materia {0}!!!", m.Nombre);
+            }
+            
+            return m;
+        }
+        public static Materia operator -(Materia m, Alumno a)
+        {
+            if(m==a)
+            {
+                m.Alumnos.Remove(a);
+                Console.WriteLine("Se quito el alumno de la materia {0}!!!", m.Nombre);
+            }
+            else
+            {
+                Console.WriteLine("El alummno no esta en la materia {0}!!!", m.Nombre);
+            }
+            return m;
+        }
+        #endregion
+
+
+    }
+}
